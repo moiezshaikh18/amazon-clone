29cbb0e9853bd73615e3ac765b901be49c029bee
moiz <moiezshaikh18@gmail.com>
2022-03-21 23:11:38 +0530

    working on product

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 const Home = () => {
   return (
     <div className="home">
@@ -7,6 +8,14 @@ const Home = () => {
         className="home_image"
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt=""
+      />
+
+      <Product
+        id="12321341"
+        title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+        price={11.96}
+        rating={5}
+        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
       />
     </div>
   );
